81d5adf5ad50ae0f57cf6533263f984d724a023c
perezvon <john.a.maloney@gmail.com>
2017-03-02 13:25:45 -0600

    initial commit

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 'use strict';
 
-import {convertToUnix, convertToDateStr} from './timeConvert.js'
-
-import express from 'express'
+var moment = require('moment')
+var express = require('express')
 
 const app = express()
 
@@ -10,18 +9,27 @@ const port = process.env.PORT || 3000;
 
 app.route('/')
 	.get((req, res) => {
-      res.sendFile(process.cwd() + './index.html');
+      res.sendFile(process.cwd() + '/index.html');
     });
 
 app.get('/:query', (req, res) => {
-	const query = this.props.params.query;
+	const query = req.params.query;
 	let unix = null, dateStr = null;
 	!Number.isNaN(+query) ? dateStr = convertToDateStr(query) : dateStr = moment(query, "MMMM D, YYYY").isValid() ? moment(query, "MMMM D, YYYY") : null;
 	Number.isNaN(+query) && moment(query, "MMMM D, YYYY").isValid() ? unix = convertToUnix(query) : unix = +query;
 	const result = {"unix": unix, "dateStr": dateStr};
-	return JSON.stringify(result);
+	res.json(result);
 })
 
 app.listen(port, function() {
     console.log('Timestamp app listening on port ' + port);
 });
+
+
+function convertToUnix (dateStr) {
+	return moment(dateStr, "MMMM D, YYYY").format("X");
+}
+
+function convertToDateStr(unix) {
+	return moment(unix, "X").format("MMMM D, YYYY");
+}
--- timeConvert.js
+++
@@ -1,9 +0,0 @@
-import moment from 'moment'
-
-export function convertToUnix = (dateStr) => {
-	return moment(dateStr);
-}
-
-export function convertToDateStr = (unix) => {
-	return moment(unix).format("MMDDYYYY");
-}

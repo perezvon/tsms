81ce7b7c9112944da31a434f77ffc439a4836635
perezvon <john.a.maloney@gmail.com>
2017-03-02 13:35:56 -0600

    natural date format fix

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.route('/')
 app.get('/:query', (req, res) => {
 	const query = req.params.query;
 	let unix = null, dateStr = null;
-	!Number.isNaN(+query) ? dateStr = convertToDateStr(query) : dateStr = moment(query, "MMMM D, YYYY").isValid() ? moment(query, "MMMM D, YYYY") : null;
+	!Number.isNaN(+query) ? dateStr = convertToDateStr(query) : dateStr = moment(query, "MMMM D, YYYY").isValid() ? moment(query).format("MMMM D, YYYY") : null;
 	Number.isNaN(+query) && moment(query, "MMMM D, YYYY").isValid() ? unix = convertToUnix(query) : unix = +query;
 	const result = {"unix": unix, "dateStr": dateStr};
 	res.json(result);
